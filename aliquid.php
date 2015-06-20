58351ccb7d18dabf8a1c30f259ab4caa1a360e05
klausner <klausner17@gmail.com>
2015-06-20 17:09:47 -0300

    Layout definido para a tela aliquid

--- aliquid.php
+++ aliquid.php
@@ -6,20 +6,19 @@
 		<link href="css/customstyle.css" rel="stylesheet">
 	</head>
 	<body>
-		<div class="container-fluid back-container medium">
-			<div class="span2" style="background-color:blue;">
-				<a href="aaa">Abrir novo RNC</a><br>
-				<a href="aaa">Consultar RNC</a><br>
-				<a href="aaa">Abrir novo RRC</a><br>
-				<a href="aaa">Consultar RRC</a><br>
-				<a href="aaa">Produto e peças</a><br>
-				<a href="aaa">Clientes</a><br>
-				<a href="aaa">Relatórios</a><br>
-				<a href="aaa">Usuários</a><br>
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<div class="span2 col-sm-3 menu-lateral">
+					<p>eeee</p>
+				</div>
+				<div class="span10 col-sm-9 conteudo">
+					eeeee<br>
+					eeeee<br>
+					eeeee<br>
+					eeeee<br>
+					eeeee<br>eeeee<br>eeeee<br>eeeee<br>eeeee<br>eeeee<br>eeeee<br>eeeee<br>eeeee<br>eeeee<br>eeeee<br>eeeee<br>eeeee<br>eeeee<br>eeeee<br>eeeee<br>eeeee<br>eeeee<br>
+				</div>
 			</div>
-			<div class="span10">
-			eeee
-			</div>		
 		</div>
 	</body>
 </html>
